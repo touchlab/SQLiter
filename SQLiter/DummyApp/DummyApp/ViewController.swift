148b3192ee3a43e52e8b4e47715407539803e772
Kevin Galligan <kgalligan@gmail.com>
2018-11-22 23:47:45 -0500

    Added dummy app for ms app center

---
+++ SQLiter/DummyApp/DummyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DummyApp
+//
+//  Created by Kevin Galligan on 11/22/18.
+//  Copyright © 2018 Kevin Galligan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
