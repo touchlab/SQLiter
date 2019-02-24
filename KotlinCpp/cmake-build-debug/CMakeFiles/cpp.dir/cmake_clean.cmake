b69dbb233a2292493f7fb02d77d69682a17d2841
Kevin Galligan <kgalligan@gmail.com>
2019-02-24 14:26:08 -0500

    Version 0.5.13

--- KotlinCpp/cmake-build-debug/CMakeFiles/cpp.dir/cmake_clean.cmake
+++ KotlinCpp/cmake-build-debug/CMakeFiles/cpp.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/android_database_CursorWindow.cpp.o"
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/android_database_SQLiteCommon.cpp.o"
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/android_database_SQLiteConnection.cpp.o"
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/android_database_SQLiteGlobal.cpp.o"
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/AndroidfwCursorWindow.cpp.o"
-  "CMakeFiles/cpp.dir/knarch/src/main/cpp/SQLiteSupport.cpp.o"
+  "CMakeFiles/cpp.dir/knarch/src/main/cpp/SQLiteCommon.cpp.o"
   "CMakeFiles/cpp.dir/knarch/src/main/cpp/KonanHelper.cpp.o"
   "cpp.pdb"
   "cpp"
--- KotlinCpp/cmake-build-debug/cmake_install.cmake
+++ KotlinCpp/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/kgalligan/temp6/kmpsql/KotlinCpp
+# Install script for directory: /Users/kgalligan/devel_kmp/kmpsql/KotlinCpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/kgalligan/temp6/kmpsql/KotlinCpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/kgalligan/devel_kmp/kmpsql/KotlinCpp/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
