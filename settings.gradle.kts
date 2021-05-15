86950891153169f037e11a8eaa2f4629c8322d34
Kevin Galligan <kgalligan@gmail.com>
2021-05-14 21:12:11 -0400

    Clean up config and v1.0.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,15 @@ rootProject.name = "sqliter"
 include(":sqliter-driver")
 
 pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
-    }
+  repositories {
+    google()
+    gradlePluginPortal()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots/")
+  }
+  val KOTLIN_VERSION: String by settings
+  plugins {
+    kotlin("multiplatform") version KOTLIN_VERSION
+  }
 }
 
 enableFeaturePreview("GRADLE_METADATA")
--- sqliter-driver/build.gradle.kts
+++ sqliter-driver/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.5.0"
+    kotlin("multiplatform")
 }
 
 val GROUP: String by project
